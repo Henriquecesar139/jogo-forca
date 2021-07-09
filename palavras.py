789e046525b9b8806c0ed8b4da1a87d2ff154dda
Henriquecesar139 <henriquepb139@gmail.com>
2021-07-09 20:43:11 -0300

    Arquivo com as palavras

---
+++ palavras.py
@@ -0,0 +1,28 @@
+#palavras usadas no jogo da forca
+
+limao = ['l', 'i', 'm', 'a', 'o']
+pera = ['p', 'e', 'r', 'a']
+uva = ['u', 'v', 'a']
+jambo = ['j', 'a', 'm', 'b', 'o']
+caqui = ['c', 'a', 'q', 'u', 'i']
+cidra = ['c', 'i', 'd', 'r', 'a']
+figo = ['f', 'i', 'g', 'o']
+groselha = ['g', 'r', 'o', 's', 'e', 'l', 'h', 'a']
+melao = ['m', 'e', 'l', 'a', 'o']
+pequi = ['p', 'e', 'q', 'u', 'i']
+pitomba = ['p', 'i', 't', 'o', 'm', 'b', 'a']
+
+#Lista com todas as palavras
+frutas = [
+    limao,
+    pera,
+    uva,
+    jambo,
+    caqui,
+    cidra,
+    figo,
+    groselha,
+    melao,
+    pequi,
+    pitomba
+]

311def75580591296f73c80aa986b94e39d7f447
Henriquecesar139 <henriquepb139@gmail.com>
2021-07-12 18:29:28 -0300

    Novas palavras adicionadas

--- palavras.py
+++ palavras.py
@@ -1,5 +1,17 @@
 #palavras usadas no jogo da forca
 
+maca = ['m', 'a', 'c', 'a'] 
+banana = ['b', 'a', 'n', 'a', 'n', 'a']
+abacaxi = ['a', 'b', 'a', 'c', 'a', 'x', 'i']
+jaca = ['j', 'a', 'c', 'a']
+coco = ['c', 'o', 'c', 'o']
+goiaba = ['g', 'o', 'i', 'a', 'b', 'a']
+jabuticaba = ['j', 'a', 'b', 'u', 't', 'i', 'c', 'a', 'b', 'a']
+mamao = ['m', 'a', 'm', 'a', 'o']
+mangaba = ['m', 'a', 'n', 'g', 'a', 'b', 'a']
+amora = ['a', 'm', 'o', 'r', 'a']
+acerola = ['a', 'c', 'e', 'r', 'o', 'l', 'a']
+castanha = ['c', 'a', 's', 't', 'a', 'n', 'h', 'a']
 limao = ['l', 'i', 'm', 'a', 'o']
 pera = ['p', 'e', 'r', 'a']
 uva = ['u', 'v', 'a']
@@ -18,6 +30,18 @@ cambuci = ['c', 'a', 'm', 'b', 'u', 'c', 'i']
 
 #Lista com todas as palavras
 frutas = [
+    maca,
+    banana,
+    abacaxi,
+    jaca,
+    coco,
+    goiaba,
+    jabuticaba,
+    mamao,
+    mangaba,
+    amora,
+    acerola,
+    castanha,
     limao,
     pera,
     uva,

6c4d3322173f85a99647cce91fefca35c4c33a1b
Henriquecesar139 <henriquepb139@gmail.com>
2021-07-13 12:13:27 -0300

    Adiciona mais frutas

--- palavras.py
+++ palavras.py
@@ -27,9 +27,23 @@ noz = ['n', 'o', 'z']
 caju = ['c' ,'a', 'j', 'u']
 jenipapo = ['j', 'e', 'n', 'i', 'p', 'a', 'p', 'o']
 cambuci = ['c', 'a', 'm', 'b', 'u', 'c', 'i']
+sapoti = ['s', 'a', 'p', 'o', 't', 'i']
+ameixa = ['a', 'm', 'e', 'i', 'x', 'a']
+bacaba = ['b', 'a', 'c', 'a', 'b', 'a']
+cacau = ['c', 'a' ,'c', 'a', 'u']
+carambola = ['c', 'a', 'r', 'a', 'm', 'b', 'o', 'l', 'a']
+framboesa = ['f', 'r', 'a', 'm', 'b', 'o', 'e', 's', 'a']
+kiwi = ['k', 'i', 'w', 'i']
+laranja = ['l', 'a', 'r', 'a', 'n', 'j', 'a']
 
 #Lista com todas as palavras
 frutas = [
+    bacaba,
+    cacau,
+    carambola,
+    framboesa,
+    kiwi,
+    laranja,
     maca,
     banana,
     abacaxi,
@@ -56,5 +70,7 @@ frutas = [
     noz,
     caju,
     jenipapo,
-    cambuci
+    cambuci,
+    sapoti,
+    ameixa,
 ]

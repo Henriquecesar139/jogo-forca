a3b0458e7a0250a674534a31566eb1a6b14fd84e
henriquecesar139 <henriquecesar1395@gmail.com>
2022-03-05 12:03:24 -0300

    Otimização

--- palavras.py
+++ palavras.py
@@ -1,166 +1,90 @@
 #palavras usadas no jogo da forca
 
-#frutas
-maca = ['m', 'a', 'c', 'a'] 
-banana = ['b', 'a', 'n', 'a', 'n', 'a']
-abacaxi = ['a', 'b', 'a', 'c', 'a', 'x', 'i']
-jaca = ['j', 'a', 'c', 'a']
-coco = ['c', 'o', 'c', 'o']
-goiaba = ['g', 'o', 'i', 'a', 'b', 'a']
-jabuticaba = ['j', 'a', 'b', 'u', 't', 'i', 'c', 'a', 'b', 'a']
-mamao = ['m', 'a', 'm', 'a', 'o']
-mangaba = ['m', 'a', 'n', 'g', 'a', 'b', 'a']
-amora = ['a', 'm', 'o', 'r', 'a']
-acerola = ['a', 'c', 'e', 'r', 'o', 'l', 'a']
-castanha = ['c', 'a', 's', 't', 'a', 'n', 'h', 'a']
-limao = ['l', 'i', 'm', 'a', 'o']
-pera = ['p', 'e', 'r', 'a']
-uva = ['u', 'v', 'a']
-jambo = ['j', 'a', 'm', 'b', 'o']
-caqui = ['c', 'a', 'q', 'u', 'i']
-cidra = ['c', 'i', 'd', 'r', 'a']
-figo = ['f', 'i', 'g', 'o']
-groselha = ['g', 'r', 'o', 's', 'e', 'l', 'h', 'a']
-melao = ['m', 'e', 'l', 'a', 'o']
-pequi = ['p', 'e', 'q', 'u', 'i']
-pitomba = ['p', 'i', 't', 'o', 'm', 'b', 'a']
-noz = ['n', 'o', 'z']
-caju = ['c' ,'a', 'j', 'u']
-cambuci = ['c', 'a', 'm', 'b', 'u', 'c', 'i']
-sapoti = ['s', 'a', 'p', 'o', 't', 'i']
-ameixa = ['a', 'm', 'e', 'i', 'x', 'a']
-cacau = ['c', 'a' ,'c', 'a', 'u']
-carambola = ['c', 'a', 'r', 'a', 'm', 'b', 'o', 'l', 'a']
-framboesa = ['f', 'r', 'a', 'm', 'b', 'o', 'e', 's', 'a']
-kiwi = ['k', 'i', 'w', 'i']
-laranja = ['l', 'a', 'r', 'a', 'n', 'j', 'a']
-
-
-#Animais
-cachorro = ['c', 'a', 'c', 'h', 'o', 'r', 'r', 'o']
-gato = ['g', 'a', 't', 'o']
-peixe = ['p', 'e', 'i', 'x', 'e']
-zebra = ['z', 'e', 'b', 'r', 'a']
-camaleao = ['l' ,'e', 'a', 'o']
-leao = ['l', 'e', 'a', 'o']
-lobo = ['l', 'o', 'b', 'o']
-avestruz = ['a', 'v', 'e', 's', 't', 'r', 'u', 'z']
-cavalo = ['c', 'a', 'v', 'a', 'l', 'o']
-camelo = ['c', 'a', 'm', 'e', 'l', 'o']
-tubarao = ['t', 'u', 'b', 'a', 'r', 'a', 'o']
-boi = ['b', 'o', 'i']
-tigre = ['t', 'i', 'g', 'r', 'e']
-macaco = ['m', 'a', 'c', 'a', 'c', 'o']
-cobra = ['c', 'o', 'b', 'r', 'a']
-lagarto = ['l', 'a', 'g', 'a', 'r', 't', 'o']
-cabra = ['c', 'a', 'b', 'r', 'a']
-vaca = ['v', 'a', 'c', 'a']
-ganso = ['g', 'a', 'n', 's', 'o']
-corvo = ['c', 'o', 'r', 'v', 'o']
-rato = ['r', 'a', 't', 'o']
-
-#paises
-brasil = ['b', 'r', 'a', 's', 'i', 'l']
-venezuela = ['v', 'e', 'n', 'e', 'z', 'u', 'e', 'l', 'a']
-paraguai = ['p', 'a', 'r', 'a', 'g', 'u', 'a', 'i']
-uruguai = ['u', 'r', 'u', 'g', 'u', 'a', 'i']
-chile = ['c', 'h', 'i', 'l', 'e']
-argentina = ['a', 'r', 'g', 'e', 'n', 't', 'i', 'n', 'a']
-peru = ['p', 'e', 'r', 'u']
-haiti = ['h', 'a', 'i', 't', 'i']
-portugal = ['p', 'o', 'r', 't', 'u', 'g', 'a', 'l']
-japao = ['j', 'a', 'p', 'a', 'o']
-china = ['c', 'h', 'i', 'n', 'a']
-alemanha = ['a', 'l', 'e', 'm', 'a', 'n', 'h', 'a']
-russia = ['r', 'u', 's', 's', 'i', 'a']
-polonia = ['p', 'o', 'l', 'o', 'n', 'i', 'a']
-mexico = ['m', 'e', 'x', 'i', 'c', 'o']
-canada = ['c', 'a', 'n', 'a', 'd', 'a']
-franca = ['f', 'r', 'a', 'n', 'c', 'a']
-bolivia = ['b', 'o', 'l', 'i', 'v', 'i', 'a']
-colombia = ['c', 'o', 'l', 'o', 'm', 'b', 'i', 'a']
 
 #Lista com as frutas
 frutas = [
-    cacau,
-    carambola,
-    framboesa,
-    kiwi,
-    laranja,
-    maca,
-    banana,
-    abacaxi,
-    jaca,
-    coco,
-    goiaba,
-    jabuticaba,
-    mamao,
-    mangaba,
-    amora,
-    acerola,
-    castanha,
-    limao,
-    pera,
-    uva,
-    jambo,
-    caqui,
-    cidra,
-    figo,
-    groselha,
-    melao,
-    pequi,
-    pitomba,
-    noz,
-    caju,
-    cambuci,
-    sapoti,
-    ameixa,
+    'c a c a u',
+    'c a r a m b o l a',
+    'f r a m b o e s a',
+    'k i w i',
+    'l a r a n j a',
+    'm a c a',
+    'b a n a n a',
+    'a b a c a x i',
+    'j a c a',
+    'c o c o',
+    'g o i a b a',
+    'j a b u t i c a b a',
+    'm a m a o',
+    'm a n g a b a',
+    'a m o r a',
+    'a c e r o l a',
+    'c a s t a n h a',
+    'l i m a o',
+    'p e r a',
+    'u v a',
+    'j a m b o',
+    'c a q u i',
+    'c i d r a',
+    'f i g o',
+    'g r o s e l h a',
+    'm e l a o',
+    'p e q u i',
+    'p i t o m b a',
+    'n o z',
+    'c a j u',
+    'c a m b u c i',
+    's a p o t i',
+    'a m e i x a',
 ]
 
 #Lista com os animais
 animais = [
-    cachorro, 
-    gato,
-    peixe,
-    zebra,
-    camaleao,
-    leao,
-    lobo,
-    avestruz,
-    cavalo,
-    camelo,
-    tubarao,
-    boi,
-    tigre,
-    macaco,
-    cobra,
-    lagarto,
-    cabra,
-    vaca,
-    ganso,
-    corvo,
-    rato
+    'c a c h o r r o', 
+    'g a t o',
+    'p e i x e',
+    'z e b r a',
+    'c a m a l e a o',
+    'l e a o',
+    'l o b o',
+    'a v e s t r u z',
+    'c a v a l o',
+    'c a m e l o',
+    't u b a r a o',
+    'b o i',
+    't i g r e',
+    'm a c a c o',
+    'c o b r a',
+    'l a g a r t o',
+    'c a b r a',
+    'v a c a',
+    'g a n s o',
+    'c o r v o',
+    'r a t o'
 
 ]
 
 paises = [
-    brasil,
-    venezuela,
-    paraguai,
-    uruguai,
-    chile,
-    argentina,
-    peru,
-    haiti,
-    portugal,
-    japao,
-    china,
-    alemanha,
-    russia,
-    polonia,
-    mexico,
-    canada,
-    franca,
-    bolivia,
-    colombia
+    'b r a s i l',
+    'v e n e z u e l a',
+    'p a r a g u a i',
+    'u r u g u a i',
+    'c h i l e',
+    'a r g e n t i n a',
+    'p e r u',
+    'h a i t i',
+    'p o r t u g a l',
+    'j a p a o',
+    'c h i n a',
+    'a l e m a n h a',
+    'r u s s i a',
+    'p o l o n i a',
+    'm e x i c o',
+    'c a n a d a',
+    'f r a n c a',
+    'b o l i v i a',
+    'c o l o m b i a',
+    'u c r a n i a',
+    'c a n a d a',
+    'n o r u e g a',
 ]
